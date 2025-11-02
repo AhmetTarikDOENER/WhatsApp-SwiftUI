ab05244b0a8178c240bd2e771488f40a7243b468
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-11-02 12:47:49 +0300

    Initial commit that indicates the starting point of the WhatsApp-SwiftUI project.

---
+++ WhatsApp-SwiftUI/WhatsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_SwiftUIApp.swift
+//  WhatsApp-SwiftUI
+//
+//  Created by Ahmet Tarik DÖNER on 2.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
