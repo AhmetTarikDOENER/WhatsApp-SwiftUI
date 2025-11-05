a207c13e949613c8bb9abf41adb7cf34e3b0d98a
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-11-05 18:11:47 +0300

    Added a function called chatPartnerRowView(user:) that returns the related row views. Embedded that views into a button to be able to make that clickable.

--- WhatsApp-SwiftUI/Screen/Core/Channel/View/Other/AddGroupChatPartnerView.swift
+++ WhatsApp-SwiftUI/Screen/Core/Channel/View/Other/AddGroupChatPartnerView.swift
@@ -2,17 +2,37 @@ import SwiftUI
 
 struct AddGroupChatPartnerView: View {
     
+    //  MARK: - Property
     @State private var searchText = ""
     
     var body: some View {
         List {
-            Text("TEXT")
+            Section {
+                ForEach(0 ..< 11) { _ in
+                    Button {
+                        print("Selected")
+                    } label: {
+                        chatPartnerRowView(user: .placeholder)
+                    }
+                }
+            }
         }
         .searchable(
             text: $searchText,
             placement: .navigationBarDrawer(displayMode: .always),
             prompt: "Search name or number"
         )
+    }
+    
+    //  MARK: - Private
+    private func chatPartnerRowView(user: UserItem) -> some View {
+        ChatPartnerRowView(user: .placeholder) {
+            Spacer()
+            
+            Image(systemName: "circle")
+                .foregroundStyle(Color(.systemGray4))
+                .imageScale(.large)
+        }
     }
 }
 
