6371b284b597c14d0aa15baed89932cad5bdd7cc
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-11-25 19:56:35 +0300

    Added another method on AudioRecorderService called startTimer(). Added a property called timer of type AnyCancellable and assign to the publisher on Timer object that publishes every 1 seconds. Also added a property called startingTime of type Date and elapsedTime of type TimeInterval.

--- WhatsApp-SwiftUI/Service/AudioRecorderService.swift
+++ WhatsApp-SwiftUI/Service/AudioRecorderService.swift
@@ -1,11 +1,16 @@
 import Foundation
 import AVFoundation
+import Combine
 
 final class AudioRecorderService {
     
     private var audioRecorder: AVAudioRecorder?
     private var isRecording = false
+    private var startingTime: Date?
+    private var timer: AnyCancellable?
+    private var elapsedTime: TimeInterval = 0
     
+    /// Setup audio session + define where to store + settings + start recording
     func startRecording() {
         let session = AVAudioSession.sharedInstance()
         do {
@@ -13,7 +18,7 @@ final class AudioRecorderService {
             try session.overrideOutputAudioPort(.speaker)
             try session.setActive(true)
         } catch {
-            print("AudioRecorderService -> Failed to setup AVAudioSession")
+            print("❌ AudioRecorderService -> Failed to setup AVAudioSession")
         }
         
         let docPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
@@ -31,12 +36,23 @@ final class AudioRecorderService {
             audioRecorder = try AVAudioRecorder(url: audioFileURL, settings: settings)
             audioRecorder?.record()
             isRecording = true
+            startingTime = Date()
+            startTimer()
         } catch {
-            print("AudioRecorderService -> Failed to setup AVAudioRecorder")
+            print("❌ AudioRecorderService -> Failed to setup AVAudioRecorder")
         }
     }
     
     func stopRecording() {
         
+    }
+    
+    private func startTimer() {
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let startingTime = self?.startingTime else { return }
+                self?.elapsedTime = Date().timeIntervalSince(startingTime)
+            }
     }
 }
