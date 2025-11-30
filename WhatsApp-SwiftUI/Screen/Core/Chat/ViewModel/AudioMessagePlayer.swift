341201ba3ceaf192ad8816b0e2011db49d1d47d5
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-11-30 14:36:51 +0300

    Added an object called AudioMessagePlayer that conforms to the ObservableObject protocol, which is kind of acting like a ViewModel object.

---
+++ WhatsApp-SwiftUI/Screen/Core/Chat/ViewModel/AudioMessagePlayer.swift
@@ -0,0 +1,96 @@
+import Foundation
+import AVFoundation
+
+final class AudioMessagePlayer: ObservableObject {
+    
+    //  MARK: - Properties
+    private var player: AVPlayer?
+    private var currentAudioURL: URL?
+    private var playerItem: AVPlayerItem?
+    private var playbackState = PlaybackState.stopped
+    private var currentTime = CMTime.zero
+    private var currentTimeObserver: Any?
+    
+    //  MARK: - Deinit
+    deinit { removeObservers() }
+    
+    func playAudio(from url: URL) {
+        if let currentAudioURL, currentAudioURL == url {
+            /// Resumes to playing that already playing
+            resumePlaying()
+        } else {
+            /// Plays an audio message
+            currentAudioURL = url
+            let playerItem = AVPlayerItem(url: url)
+            self.playerItem = playerItem
+            player = AVPlayer(playerItem: playerItem)
+            player?.play()
+            playbackState = .playing
+            observeCurrentPlayerTime()
+            observeEndOfPlayback()
+        }
+    }
+    
+    func pauseAudio() {
+        player?.pause()
+        playbackState = .paused
+    }
+    
+    func seek(to timeInterval: TimeInterval) {
+        guard let player else { return }
+        let targetTime = CMTime(seconds: timeInterval, preferredTimescale: 1)
+        player.seek(to: targetTime)
+    }
+    
+    //  MARK: - Private
+    private func observeCurrentPlayerTime() {
+        currentTimeObserver = player?.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1), queue: .main) { [weak self] time in
+            self?.currentTime = time
+            print("observeCurrentPlayerTime: \(time)")
+        }
+    }
+    
+    private func observeEndOfPlayback() {
+        NotificationCenter.default.addObserver(
+            forName: AVPlayerItem.didPlayToEndTimeNotification,
+            object: player?.currentItem,
+            queue: .main) { [weak self] _ in
+                self?.stopAudioPlayer()
+                print("observeEndOfPlayback")
+            }
+    }
+    
+    private func stopAudioPlayer() {
+        player?.pause()
+        player?.seek(to: .zero)
+        playbackState = .stopped
+        currentTime = .zero
+    }
+    
+    private func resumePlaying() {
+        if playbackState == .paused || playbackState == .stopped {
+            player?.play()
+            playbackState = .playing
+        }
+    }
+    
+    private func removeObservers() {
+        guard let currentTimeObserver else { return }
+        player?.removeTimeObserver(currentTimeObserver)
+        self.currentTimeObserver = nil
+        print("removeObservers fired")
+    }
+    
+    private func tearDown() {
+        removeObservers()
+        player = nil
+        playerItem = nil
+        currentAudioURL = nil
+    }
+}
+
+extension AudioMessagePlayer {
+    enum PlaybackState {
+        case stopped, playing, paused
+    }
+}
