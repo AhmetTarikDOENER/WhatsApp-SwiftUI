0a46f7ed897a758850c66c353e2b4b56f0b2bac6
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2025-12-07 22:51:19 +0300

    Started to build push notification. In AppDelegate added a helper method called setupPushNotification(for application:). Inside method body stored the notification related activitites on a property called notificationCenter of type UNUserNotificationCenter. Also stored the remote-local notification options on options property. And then called .requestAuthorization(options:,completionHandler:) over notificationCenter inside closure body registered application for remote notifications. Also assigned the delegate property of notificationCenter to the current instance which extended the AppDelegate with didRegisterForRemoteNotificationsWithDeviceToken and willPresent methods. Inside didRegisterForRemoteNotificationsWithDeviceToken method setted Firebase Messaging's apnsToken to the device token. didReceiveRegistrationToken method gives us the firebase cloud messaging token.

--- WhatsApp-SwiftUI/App/WhatsApp_SwiftUIApp.swift
+++ WhatsApp-SwiftUI/App/WhatsApp_SwiftUIApp.swift
@@ -1,14 +1,17 @@
 import SwiftUI
+import FirebaseCore
 import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
     
-      configureFirebase()
-
+        configureFirebase()
+        setupPushNotification(for: application)
+        
     return true
   }
     
@@ -23,6 +26,47 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         options.storageBucket = FirebaseConfig.storageBucket
         
         FirebaseApp.configure(options: options)
+    }
+    
+    private func setupPushNotification(for application: UIApplication) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        Messaging.messaging().delegate = self
+        notificationCenter.delegate = self
+        
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+        
+        notificationCenter.requestAuthorization(options: options) { granted, error in
+            if let error {
+                print("APNS Auth error: \(error.localizedDescription)")
+                return
+            }
+            
+            if granted {
+                print("APNS Auth Granted")
+            } else {
+                print("APNS Auth Failed")
+            }
+            
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+    }
+}
+
+//  MARK: - UNUserNotificationCenterDelegate & MessagingDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS successfully registered for PN with device token")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        return [.banner, .badge, .sound]
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("APNS device token is: \(fcmToken)")
     }
 }
 
